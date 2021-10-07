1dcb1c56353b85060419604727a3e6b98799dc99
mcamillosf <matheuscamillo@hotmail.com.br>
2021-10-07 19:58:36 -0300

    Updates

---
+++ precache-manifest.7bd7e74deabc9bc498740941c16d381c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8101edfff31b49ec5def8c7852cf209b",
+    "url": "/mcamillosf.github.io/recipes-app/index.html"
+  },
+  {
+    "revision": "52e201493db91ed93602",
+    "url": "/mcamillosf.github.io/recipes-app/static/css/main.32eef023.chunk.css"
+  },
+  {
+    "revision": "110b863a748c43724bdc",
+    "url": "/mcamillosf.github.io/recipes-app/static/js/2.444e89ff.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/mcamillosf.github.io/recipes-app/static/js/2.444e89ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e201493db91ed93602",
+    "url": "/mcamillosf.github.io/recipes-app/static/js/main.42bf617a.chunk.js"
+  },
+  {
+    "revision": "54c901cbcb6116cb1aec",
+    "url": "/mcamillosf.github.io/recipes-app/static/js/runtime-main.ce12c102.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "5eeb733d4e71174ca63f44ffde8def55",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/loginIcon.5eeb733d.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/mcamillosf.github.io/recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
